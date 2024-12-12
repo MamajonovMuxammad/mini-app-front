736a815292f7a5710585e1d2f5af2fc964718435
Muxammad <ferzadrotttmagister@gmail.com>
2024-12-12 23:12:33 +0500

    second-commit-bottom

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { FaArrowLeft, FaPhone } from 'react-icons/fa'; // Иконки
+import { FaPlus, FaSmile, FaStream, FaMicrophone, FaArrowRight } from 'react-icons/fa';
+import './App.css'; // Подключение CSS файла
 
 function MainContent({ activeSection }) {
   return (
     <div className="main-content">
-      {/* Верхний левый угол (кнопка назад) */}
-      <div className="back-btn">
-        <FaArrowLeft />
-      </div>
-      
-      {/* Верхний правый угол (иконка телефона) */}
-      <div className="phone-btn">
-        <FaPhone />
+      {/* Верхняя часть интерфейса */}
+      <div className="content-header">
+        <h1>Текущий раздел: {activeSection}</h1>
       </div>
 
-      {/* Основной контент в зависимости от активного раздела */}
-      <div>
-        <h1>{activeSection}</h1>
-        {/* Дополнительный контент */}
+      {/* Нижняя панель */}
+      <div className="bottom-panel">
+        <div className="right-group">
+          <button className="bottom-btn">
+            <FaPlus />
+          </button>
+          <button className="bottom-btn">
+            <FaSmile />
+          </button>
+          <button className="bottom-btn">
+            <FaStream />
+          </button>
+        </div>
+        <div className="left-group">
+          <button className="bottom-btn">
+            <FaMicrophone />
+          </button>
+          <button className="bottom-btn">
+            <FaArrowRight />
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { FaBell, FaUser } from 'react-icons/fa'; // Иконки уведомлений и пользователя
-
+import './App.css';
 function TopBar() {
   return (
     <div className="topbar">
       <div className="left">
-        <button className="btn">Люди</button>
+        <button className="btn-people">Люди</button>
       </div>
       <div className="center">
         <span className="nickname">Никнейм</span>
